c8200eb1f42162a473ea80c67606c8f4f596f887
Richard Polzin <polzin.richard@gmail.com>
2016-09-18 11:33:28 +0200

    updated to latest dev version before break

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 ﻿# Encoding: Utf-8
-import os
-import time
 import random
+from collections import OrderedDict, deque
 import keys  # create a keys.py file with your twitter tokens if you want to run your own instance !
-import twitter
-from collections import OrderedDict
+from TwitterAPI import TwitterAPI, TwitterRequestError
 
 intro = [("How about a","?"), ("Why not try the","?"), ("Try a","!"), ("Check out a","!"), ("Nothing like a",".")]
 multi = ["","Single", "Double", "Tripple", "Quad"]
 size = ["Short", "Tall", "Grande", "Venti® Hot", "Venti® Cold", "Trenta® Cold"]
 coffee = ["Espresso", "Espresso Macchiato", "Espresso con Panna", "Caffe Americano", "Cappuccino", "Caffe Latte", "Vanilla Latte", "Caramel Macchiato", "Chocolate Mocha", "White Caffe Mocha", "Frappuccino", "Ristretto", "Chai Tea Latte"]
-attribute = ["","Non-Fat", "Iced", "Sugar Free", "Venti", "Soy", "No Foam", "Triple", "Half Sweet", "Decaf", "Half-Caff" , "Quad", "One-Pump", "Skinny", "Sugar-Free Syrup", "Light Ice", "No Whip", "Dolce Soy"]
-syrup_type = ["","Extra Hot", "Non-Fat", "Half-Sweet", "One-Pump", "Ten-Pump", "4-Pump"]
+attribute = ["","Non-Fat", "Iced", "Sugar Free", "Venti", "Soy", "No Foam", "Tripple", "Half Sweet", "Decaf", "Half-Caff" , "Quad", "One-Pump", "Skinny", "Sugar-Free Syrup", "Light Ice", "No Whip", "Dolce Soy"]
+syrup_type = ["","With Extra Hot", " And Non-Fat", " On Half-Sweet", " Add One-Pump", "Add Ten-Pump", "And 4-Pump"]
 syrup = ["", "Caramel", "Hazelnut", "Cinnamon"]
-appendition = ["" ,"Extra Shot", "Extra Whip", "With An Extra Shot And Cream", "At 120 Degrees", "With Extra Whipped Cream and Chocolate Sauce"]
+appendition = ["" ,"And Extra Shot", " Plus Extra Whip", "With An Extra Shot And Cream", "At 120 Degrees", "With Extra Whipped Cream and Chocolate Sauce"]
 
 def order():
     order = OrderedDict()
@@ -35,35 +33,31 @@ def make_tweet(username):
         if len(o) < 140:
             return o
 
-def load_ids():
-    with open("ids.txt","r") as f:
-        return f.read().split(";")
-    return None
+api = TwitterAPI(keys.consumer_key, keys.consumer_secret, keys.access_token_key, keys.access_token_secret)
+bot = api.request('account/verify_credentials').json()["screen_name"]
+msgs = deque(maxlen=1000)
 
-def save_ids(ids):
-    with open("ids.txt","w") as f:
-        f.write(";".join(ids))
-
-api = twitter.Api(consumer_key=keys.consumer_key, consumer_secret=keys.consumer_secret, access_token_key=keys.access_token_key, access_token_secret=keys.access_token_secret)
-
-ids = load_ids() or []
-
-#tweets = api.GetSearch(term="Coffee", count=200)
-mentions = api.GetMentions(count=200)
-timeline = api.GetHomeTimeline(count=200)
-
-followers = api.GetFollowerIDs()
-# follow back
-for f in followers:
-    api.CreateFriendship(f,follow=True)
-
-
-for t in timeline + mentions:
-    if not t.id in ids and t.user.id in followers: # only hit up followers
-        tweet = make_tweet(t.user.screen_name)
-        ret = api.PostUpdate(tweet,in_reply_to_status_id=t.id)
-        ids.append(t.id_str)
-        print(ret)
-        break
-save_ids(ids)
-
+try:
+    for msg in api.request('user', {'replies': 'all'}):
+        if "text" in msg:
+            id = msg["id"]
+            other = msg["user"]["screen_name"]
+            to = msg["in_reply_to_screen_name"]
+            toid = msg["in_reply_to_status_id"]
+            print(other+" : "+msg["text"])
+            if other == bot:
+                msgs.append(id)
+            if to == bot and not other == bot and not toid in msgs:
+                api.request('friendships/create', {'screen_name': other})
+                t = make_tweet(other)
+                r = api.request('statuses/update', {'in_reply_to_status_id': id, 'status': t})
+                print("replied to {} with status {}".format(other, r.status_code))
+                msgs.append(id)
+            elif bot in msg["text"] and not other == bot:
+                api.request('friendships/create', {'screen_name': other})  #friend them all !
+                if random.choice([True,False]):
+                    api.request('favorites/create', {'id': id})
+                if random.randint(0,10) == 3:  # 1 in 10 chance for retweet
+                    api.request('statuses/retweet/:id', {'id': id})
+except TwitterRequestError as e:
+    print(e)
