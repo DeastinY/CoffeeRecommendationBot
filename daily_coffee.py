914a35a152eead801f544ce1c357228a26f2075f
Richard Polzin <polzin.richard@gmail.com>
2016-09-18 10:32:02 +0000

    updated script with logging and daily coffee tweet

--- daily_coffee.py
+++ daily_coffee.py
@@ -6,8 +6,6 @@ import keys  # create a keys.py file with your twitter tokens if you want to run
 import logging
 from TwitterAPI import TwitterAPI, TwitterRequestError
 
-logging.basicConfig(filename="/tmp/coffebot.log", level=logging.INFO)
-
 intro = [("How about a","?"), ("Why not try the","?"), ("Try a","!"), ("Check out a","!"), ("Nothing like a",".")]
 multi = ["","Single", "Double", "Tripple", "Quad"]
 size = ["Short", "Tall", "Grande", "Venti® Hot", "Venti® Cold", "Trenta® Cold"]
@@ -30,46 +28,22 @@ def order():
         order[random.choice(appendition)] = True
     return " ".join(" ".join(order.keys()).split())
 
-def make_tweet(username):
+def make_tweet():
     while True:
-        a, b = random.choice(intro)
-        o = u"@"+username+" "+a+" "+order()+" "+b
+        o = u"Coffee of the day :\n"+order()
         if len(o) < 140:
             return o
 
 logging.info("Connecting to Twitter API")
 api = TwitterAPI(keys.consumer_key, keys.consumer_secret, keys.access_token_key, keys.access_token_secret)
 bot = api.request('account/verify_credentials').json()["screen_name"]
-msgs = deque(maxlen=1000)  # we keep the last 1000 messages and do not reply to those again
 logging.info("Connected")
 
 try:
-    for msg in api.request('user', {'replies': 'all'}):
-        logging.info("New event")
-        logging.debug(msg)
-        if "text" in msg:
-            logging.info("Event is Tweet")
-            id = msg["id"]
-            other = msg["user"]["screen_name"]
-            to = msg["in_reply_to_screen_name"]
-            toid = msg["in_reply_to_status_id"]
-            logging.debug(other+" : "+msg["text"])
-            if other == bot:
-                logging.info("My own tweet")
-                msgs.append(id)
-            if to == bot and not other == bot and not toid in msgs:
-                logging.info("Replying to tweet directed to me !")
-                api.request('friendships/create', {'screen_name': other})
-                t = make_tweet(other)
-                r = api.request('statuses/update', {'in_reply_to_status_id': id, 'status': t})
-                logging.info("replied to {} with status {}".format(other, r.status_code))
-                msgs.append(id)
-            elif bot in msg["text"] and not other == bot:
-                logging.info("Was mentioned ! Like or Retweet maybe ?")
-                api.request('friendships/create', {'screen_name': other})  #friend them all !
-                if random.choice([True,False]):
-                    api.request('favorites/create', {'id': id})
-                if random.randint(0,10) == 3:  # 1 in 10 chance for retweet
-                    api.request('statuses/retweet/:id', {'id': id})
+    logging.info("Sending COTD")
+    t = make_tweet()
+    r = api.request('statuses/update', {'status' : t})
+    logging.info("COTD with status : {}".format(r.status_code))
+    logging.info("Done !")
 except TwitterRequestError as e:
     logging.exception(e)
