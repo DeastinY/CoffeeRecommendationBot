63b858bfda7641d0d6380baeb592bd60fdc9e799
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2016-10-02 17:14:15 +0530

    Snake casing for daily_coffee.py

--- daily_coffee.py
+++ daily_coffee.py
@@ -9,16 +9,16 @@ import logging
 import json
 from TwitterAPI import TwitterAPI, TwitterRequestError
 
-coffeeFile = open("coffeeType.json")
-coffeeTypes = json.load(coffeeFile)
-intro = coffeeTypes['intro']
-multi = coffeeTypes['multi']
-size = coffeeTypes['size']
-coffee = coffeeTypes['coffee']
-attribute = coffeeTypes['attribute']
-syrup_type = coffeeTypes['syrup_type']
-syrup = coffeeTypes['syrup']
-appendition = coffeeTypes['appendition']
+coffee_file = open("coffeeType.json")
+coffee_types = json.load(coffee_file)
+intro = coffee_types['intro']
+multi = coffee_types['multi']
+size = coffee_types['size']
+coffee = coffee_types['coffee']
+attribute = coffee_types['attribute']
+syrup_type = coffee_types['syrup_type']
+syrup = coffee_types['syrup']
+appendition = coffee_types['appendition']
 
 
 def order():
