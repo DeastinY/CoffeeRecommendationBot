15690e89625b88750a58ead00d46b9c15c2b8d11
QzSG <adrianquartx@gmail.com>
2016-10-06 12:07:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ import logging
 import json
 from TwitterAPI import TwitterAPI, TwitterRequestError
 
-try: xrange 
-except NameError: xrange = range
+try: xrange
+except NameError:
+    xrange = range
 
 logging.basicConfig(filename="/tmp/coffeebot.log", level=logging.INFO)
 
